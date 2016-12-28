738b9b2f622076fe023501bc415aa1ae24620655
Himani Rikhari <himanirikhari8@gmail.com>
2016-12-28 08:20:27 +0000

    config file and passport dependency added

---
+++ webserver/config/auth.js
@@ -0,0 +1,14 @@
+
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+
+'googleAuth' : {
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
